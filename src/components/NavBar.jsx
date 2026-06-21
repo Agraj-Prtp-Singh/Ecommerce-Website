241fef5c3ab0af0ebf09dc3e9e40faa234979a5d
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-06-21 11:30:26 +0545

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      {/* Upper Navbar */}
+      <div>
+        <div>
+          <div>
+            <a href="#">
+              <img src={Logo} alt="Brand Logo" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
