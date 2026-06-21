f749a89d7107a4e5ef61f83fce425f287282cae9
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-06-21 12:07:58 +0545

    Created Navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Logo from "../assets/logo.png";
 
 const NavBar = () => {
   return (
@@ -8,8 +9,23 @@ const NavBar = () => {
         <div>
           <div>
             <a href="#">
-              <img src={Logo} alt="Brand Logo" />
+              <img src={Logo} alt="Brand Logo" 
+              className="w-10" />
+              Shopy
             </a>
+          </div>
+          {/* search and order button */}
+          <div>
+            <div>
+              <input
+                type="text"
+                placeholder="search"
+                className="w-[200px] sm:w-[200px] hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300
+                px-2 py-1 focus:outline-none focus:border focus:border-primary
+                
+                "
+              />
+            </div>
           </div>
         </div>
       </div>
