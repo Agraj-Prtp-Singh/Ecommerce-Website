6fb53515590ee2e3c0511daaf5bf581d9ff53eb3
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-06-28 16:41:04 +0545

    Created Hero Section component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Women from "../assets/women.png";
+import shopping from "../assets/shopping.png";
+import sale from "../assets/sale.png";
+
+const imageList = [
+  {
+    id: 1,
+    img: Women,
+    title: "Upto 50% off on all Women's Wear",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente et, commodi ut recusandae dolorem provident voluptate est eligendi magni! Vitae sint aperiam perspiciatis dicta a, nemo laboriosam sequi modi ipsam",
+  },
+
+  {
+    id: 2,
+    img: shopping,
+    title: "30% off on all Women's Wear",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente et, commodi ut recusandae dolorem provident voluptate est eligendi magni! Vitae sint aperiam perspiciatis dicta a, nemo laboriosam sequi modi ipsam",
+  },
+
+  {
+    id: 3,
+    img: sale,
+    title: "70% off on all Women's Wear",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente et, commodi ut recusandae dolorem provident voluptate est eligendi magni! Vitae sint aperiam perspiciatis dicta a, nemo laboriosam sequi modi ipsam",
+  },
+];
+
+const Hero = () => {
+  return (
+    <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
+      {/* Background  pattern*/}
+      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 z-9"></div>
+      {/* hero section */}
+      <div className="container pb-8 sm:pb-0">
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2">
+            {/* text content section */}
+            <div>
+              <h1>Lorem ipsum dolor sit.</h1>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum,
+                distinctio!
+              </p>
+              <div>
+                <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+                  Order Now
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
