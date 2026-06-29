4fe4a6a510658236bd2b6628221f820e8f7d29b8
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-06-29 21:40:26 +0545

    fix: Slider component integration - use Slider instead of div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Women from "../assets/women.png";
 import shopping from "../assets/shopping.png";
 import sale from "../assets/sale.png";
+import Slider from "react-slick/lib/slider";
 
 const imageList = [
   {
@@ -30,41 +31,54 @@ const imageList = [
 ];
 
 const Hero = () => {
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: "ease-in-out",
+    pauseOnHover: false,
+    pauseOnFocus: true,
+  };
   return (
     <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
       {/* Background  pattern*/}
-      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 z-9"></div>
+      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 z-[9]"></div>
       {/* hero section */}
       <div className="container pb-8 sm:pb-0">
-        <div>
-          <div className="grid grid-cols-1 sm:grid-cols-2">
-            {/* text content section */}
-            <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left sm:order-1 order-2 relative z-10">
-              <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold bg-gradient-to-r from-primary via-primary to-secondary bg-clip-text text-transparent">
-                Lorem ipsum dolor sit.
-              </h1>
-              <p className="text-sm">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum,
-                distinctio!
-              </p>
-              <div>
-                <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
-                  Order Now
-                </button>
+        <Slider {...settings}>
+          {imageList.map((data) => (
+            <div key={data.id}>
+              <div className="grid grid-cols-1 sm:grid-cols-2">
+                {/* text content section */}
+                <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left sm:order-1 order-2 relative z-10">
+                  <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold bg-gradient-to-r from-primary via-primary to-secondary bg-clip-text text-transparent">
+                    {data.title}
+                  </h1>
+                  <p className="text-sm">{data.description}</p>
+                  <div>
+                    <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+                      Order Now
+                    </button>
+                  </div>
+                </div>
+                {/* image section  */}
+                <div className=" order-1 sm:order-2">
+                  <div className="relative z-10 ">
+                    <img
+                      src={data.img}
+                      className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto"
+                      alt=""
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-            {/* image section  */}
-            <div className="z-10 order-1 sm:order-2">
-              <div className="relative">
-                <img
-                  src={Women}
-                  className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto"
-                  alt=""
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
