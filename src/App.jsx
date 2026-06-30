271573f0e2ed444d397574d7eca41d6bdecc1bd1
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-06-30 14:39:28 +0545

    Imported Products.jsx Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero.jsx";
+import Products from "./components/Products.jsx";
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Hero />
+      <Products />
     </div>
   );
 };
