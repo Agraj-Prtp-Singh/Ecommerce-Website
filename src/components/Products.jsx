11fc2b42cb4d95f29610f55352bb320bd8aa74a0
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-06-30 14:39:06 +0545

    Created Products Component

---
+++ src/components/Products.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <div>
+        {/* Header Section */}
+        <div>
+          <p>Top Selling Products for you</p>
+          <h1>Products</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
