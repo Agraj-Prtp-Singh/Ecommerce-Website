c165edd6d80eeeea2f243f3c6230442b9b06355e
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-06-30 13:50:22 +0545

    Slider Not Working, replaced it with swiper

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import Women from "../assets/women.png";
 import shopping from "../assets/shopping.png";
 import sale from "../assets/sale.png";
-import Slider from "react-slick/lib/slider";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
 
 const imageList = [
   {
@@ -31,27 +33,25 @@ const imageList = [
 ];
 
 const Hero = () => {
-  const settings = {
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 800,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    cssEase: "ease-in-out",
-    pauseOnHover: false,
-    pauseOnFocus: true,
-  };
   return (
     <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
       {/* Background  pattern*/}
-      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 z-[9]"></div>
+      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 "></div>
       {/* hero section */}
       <div className="container pb-8 sm:pb-0">
-        <Slider {...settings}>
+        <Swiper
+          modules={[Autoplay]}
+          loop={true}
+          speed={800}
+          slidesPerView={1}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: false,
+          }}
+        >
           {imageList.map((data) => (
-            <div key={data.id}>
+            <SwiperSlide key={data.id}>
               <div className="grid grid-cols-1 sm:grid-cols-2">
                 {/* text content section */}
                 <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left sm:order-1 order-2 relative z-10">
@@ -67,7 +67,7 @@ const Hero = () => {
                 </div>
                 {/* image section  */}
                 <div className=" order-1 sm:order-2">
-                  <div className="relative z-10 ">
+                  <div className="relative">
                     <img
                       src={data.img}
                       className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto"
@@ -76,9 +76,9 @@ const Hero = () => {
                   </div>
                 </div>
               </div>
-            </div>
+            </SwiperSlide>
           ))}
-        </Slider>
+        </Swiper>
       </div>
     </div>
   );
