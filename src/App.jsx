e0e295193fa364770318ba05bccc12b6a005f01a
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-05 23:28:09 +0545

    Imported AOS Library

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero.jsx";
 import Products from "./components/Products.jsx";
+import AOS from "aos";
 
 const App = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  });
   return (
     <div>
       <NavBar />
