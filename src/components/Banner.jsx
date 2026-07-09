5f68d9718d53540eb9d9e68992faa78c50f4d9b5
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-09 17:40:25 +0545

    Completed Banner Component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,12 +8,43 @@ const Banner = () => {
   return (
     <div className="min-h-[550px] flex justify-center items-center py-12 sm:py-0">
       <div className="container">
-        <div className="gird grid-cols-1 sm:grid-cols-1 gap-6 items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 items-center">
           {/* Image Section */}
-          <div>
-            <img src={BannerImg} alt="" />
+          <div data-aos="zoom-in">
+            <img
+              src={BannerImg}
+              alt="Banner Image"
+              className="max-w-[400px] h-[350px] w-full mx-auto object-cover drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)]"
+            />
           </div>
-          {/* details section */}
+          {/* text details section */}
+          <div
+            data-aos="fade-up"
+            className="flex flex-col justify-center gap-6 sm:pt-0"
+          >
+            <h1 className="text-3xl sm:text-4xl font-bold">
+              Winter Sale upto 50% Off
+            </h1>
+            <p className="text-sm text-gray-500 tracking-wide leading-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+              incidunt, fugiat obcaecati cumque quam rem rerum debitis vel
+              distinctio nemo.
+            </p>
+            <div className="flex flex-col gap-4 ">
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <GrSecure className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-violet-100 dark:bg-violet-400" />
+                <p>Quality Products</p>
+              </div>
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <IoFastFood className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-emerald-100 dark:bg-emerald-400" />
+                <p>Fast Delivery</p>
+              </div>
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <GiFoodTruck className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-lime-100 dark:bg-lime-400" />
+                <p>Get Offers</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
