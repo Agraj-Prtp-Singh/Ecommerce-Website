017cab7f4938ecb5215ebee0669f0e4827b34f96
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-10 17:30:33 +0545

    Initialized Testimonials Component

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+
+const TestimonialData = [
+  {
+    id: 1,
+    name: "Victor",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/101/101",
+  },
+  {
+    id: 2,
+    name: "Satya Nadella",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/102/102",
+  },
+  {
+    id: 3,
+    name: "Virat Kohli",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/104/104",
+  },
+  {
+    id: 5,
+    name: "Sachin Tendulkar",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/103/103",
+  },
+];
+
+const Testimonials = () => {
+  return (
+    <div className="py-10 mb-10">
+      <div className="container">
+        {/* header section */}
+        <div className="text-center mb-10 max-w-xl mx-auto">
+          {" "}
+          <p data-aos="fade-up" className="text-sm text-primary">
+            What Our Customers Are Saying
+          </p>
+          <h1 data-aos="fade-up" className="text-3xl font-bold">
+            Testimonials
+          </h1>
+          <p data-aos="fade-up" className="text-xs text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sit eum
+            culpa?
+          </p>
+        </div>
+        {/* Testimonials Card */}
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
