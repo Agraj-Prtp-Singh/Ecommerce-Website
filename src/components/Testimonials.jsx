3fc7e2665309b65ebc8a3b06c94fb730e537134b
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-10 17:33:19 +0545

    Complete Testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
+
 import "swiper/css";
 
 const TestimonialData = [
@@ -23,7 +24,7 @@ const TestimonialData = [
     img: "https://picsum.photos/104/104",
   },
   {
-    id: 5,
+    id: 4,
     name: "Sachin Tendulkar",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
     img: "https://picsum.photos/103/103",
@@ -34,21 +35,80 @@ const Testimonials = () => {
   return (
     <div className="py-10 mb-10">
       <div className="container">
-        {/* header section */}
-        <div className="text-center mb-10 max-w-xl mx-auto">
-          {" "}
+        {/* Header */}
+        <div className="text-center mb-10 max-w-[600px] mx-auto">
           <p data-aos="fade-up" className="text-sm text-primary">
-            What Our Customers Are Saying
+            What our customers are saying
           </p>
+
           <h1 data-aos="fade-up" className="text-3xl font-bold">
             Testimonials
           </h1>
+
           <p data-aos="fade-up" className="text-xs text-gray-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sit eum
-            culpa?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+            asperiores modi Sit asperiores modi
           </p>
         </div>
-        {/* Testimonials Card */}
+
+        {/* Swiper */}
+        <div data-aos="zoom-in">
+          <Swiper
+            modules={[Autoplay]}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            speed={500}
+            spaceBetween={20}
+            loop={true}
+            breakpoints={{
+              0: {
+                slidesPerView: 1,
+              },
+              640: {
+                slidesPerView: 1,
+              },
+              768: {
+                slidesPerView: 2,
+              },
+              1024: {
+                slidesPerView: 3,
+              },
+            }}
+          >
+            {TestimonialData.map((data) => (
+              <SwiperSlide key={data.id}>
+                <div className="my-6">
+                  <div className="flex flex-col gap-4 shadow-lg py-8 px-6 rounded-xl dark:bg-gray-800 bg-primary/10 relative">
+                    <div className="mb-4">
+                      <img
+                        src={data.img}
+                        alt={data.name}
+                        className="rounded-full w-20 h-20"
+                      />
+                    </div>
+
+                    <div className="flex flex-col items-center gap-4">
+                      <div className="space-y-3">
+                        <p className="text-xs text-gray-500">{data.text}</p>
+
+                        <h1 className="text-xl font-bold text-black/80 dark:text-white">
+                          {data.name}
+                        </h1>
+                      </div>
+                    </div>
+
+                    <p className="text-black/20 text-9xl font-serif absolute top-0 right-0">
+                      ,,
+                    </p>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </div>
   );
