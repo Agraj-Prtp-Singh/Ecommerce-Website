2631d51b69b196bc36547e84d46da732abacb588
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-10 14:57:56 +0545

    Completed Subscribe Component

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Banner from "../assets/orange-pattern";
+import Banner from "../assets/green-pattern.png";
 
 const BannerImg = {
   backgroundImage: `url(${Banner})`,
@@ -11,8 +11,24 @@ const BannerImg = {
 };
 const Subscribe = () => {
   return (
-    <div data-aso="zoom-in" className=" bg-gray-100 dark:bg-gray-800 text-white     ">
-      <div></div>
+    <div
+      data-aos="zoom-in"
+      className="mb-20 bg-gray-100 dark:bg-gray-800 text-white "
+      style={BannerImg}
+    >
+      <div className="container backdrop-blur-sm py-10">
+        <div className="space-y-6 max-w-xl mx-auto">
+          <h1 className="text-2xl !text-center sm:text-left sm:text-4xl font-semibold">
+            Get Notified About New Products
+          </h1>
+          <input
+            data-aos="fade-up"
+            type="text"
+            placeholder="Enter your email"
+            className="w-full p-3 bg-white text-gray-800 dark:bg-gray-800 dark:text-white"
+          />
+        </div>
+      </div>
     </div>
   );
 };
