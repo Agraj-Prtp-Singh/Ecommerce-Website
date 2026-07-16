d03a1bdec285c8cf22ac5885f633972b0e76fbde
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-16 23:56:37 +0545

    Updated Testimonial Data

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,25 +7,25 @@ import "swiper/css";
 const TestimonialData = [
   {
     id: 1,
-    name: "Victor",
+    name: "Aryam",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
     img: "https://picsum.photos/101/101",
   },
   {
     id: 2,
-    name: "Satya Nadella",
+    name: "Bishal",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
     img: "https://picsum.photos/102/102",
   },
   {
     id: 3,
-    name: "Virat Kohli",
+    name: "Obscure",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
     img: "https://picsum.photos/104/104",
   },
   {
     id: 4,
-    name: "Sachin Tendulkar",
+    name: "Anuj",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
     img: "https://picsum.photos/103/103",
   },
@@ -56,7 +56,7 @@ const Testimonials = () => {
           <Swiper
             modules={[Autoplay]}
             autoplay={{
-              delay: 2000,
+              delay: 3000,
               disableOnInteraction: false,
               pauseOnMouseEnter: true,
             }}
